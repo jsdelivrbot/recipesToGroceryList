1ee861ff5e8d2013456a3404e071a7597a74f695
Erik Farmer <erikdfarmer@gmail.com>
2017-01-23 13:58:39 -0800

    Update reducer_recipes.js
    Increase amount of recipes

--- src/reducers/reducer_recipes.js
+++ src/reducers/reducer_recipes.js
@@ -1,38 +1,212 @@
 export default function() {
     return [
         {
-            "name": "Veggetti Alfredo",
-            "ingredients": [
-                "Zucchini",
-                "Cream Cheese",
-                "Half & Half",
-                "Parmesan",
-                "Garlic"
+            name: 'Bacon Wrapped Chicken',
+            active: false,
+            count: 0,
+            ingredients: [
+                'bacon',
+                'bacon',
+                'cheese',
+                'chicken',
+                'jalepenos',
+            ],
+            link: 'http://cavemanketo.com/lazy-keto-chicken/'
+        },
+        {
+            name: 'Blackened Pork Chops',
+            active: false,
+            count: 0,
+            ingredients: [
+                'butter',
+                'pork chops',
+            ],
+            link: 'http://cavemanketo.com/blackened-pork-chops/'
+        },
+        {
+            name: 'Breakfast for week',
+            active: false,
+            count: 0,
+            ingredients: [
+                'breakfast animal protein of choice',
+                'eggs'
+            ],
+            link: null
+        },
+        {
+            name: 'Broccoli Stir Fry',
+            active: false,
+            count: 0,
+            ingredients: [
+                'broccoli',
+                'chicken',
+                'garlic',
+                'ginger',
+                'soy sauce'
+            ],
+            link: null
+        },
+        {
+            name:' Buffalo Chicken',
+            active: false,
+            count: 0,
+            ingredients: [
+                'butter',
+                'celery/cucumber/ranch salad (optional)',
+                'chicken',
+                'franks hot sauce',
+            ],
+            link: null
+        },
+        {
+            name: 'Burgers',
+            active: false,
+            count: 0,
+            ingredients: [
+                'cheese',
+                'desired burger toppings/condiments',
+                'ground beef',
+            ],
+            link: null
+        },
+        {
+            name: 'Crock Pot Carnitas',
+            active: false,
+            count: 0,
+            ingredients: [
+                'garlic',
+                'onion',
+                'pork butt',
+            ],
+            link: 'http://cavemanketo.com/pork-carnitas/'
+        },
+        {
+            name: 'Cauliflower Stir Fry',
+            active: false,
+            count: 0,
+            ingredients: [
+                'cauliflower',
+                'chicken',
+                'soy sauce',
+            ],
+            link: null
+        },
+        {
+            name: 'Chicken and Asparagus',
+            active: false,
+            count: 0,
+            ingredients: [
+                'asparagus',
+                'chicken'
+            ],
+            link: null
+        },
+        {
+            name: 'Chicken Broccolinni',
+            active: false,
+            count: 0,
+            ingredients: [
+                'chicken',
+                'broccolinni'
             ]
         },
         {
-            "name": "Veggetti Marinara",
-            "ingredients": [
-                "Zucchini",
-                "Marinara"
-            ]
+            name: 'Caveman Chili',
+            active: false,
+            count: 0,
+            ingredients: [
+                'bacon',
+                'diced tomatoes',
+                'green bell pepper',
+                'ground pork',
+                'onion',
+                'tomato paste',
+            ],
+            link: 'http://cavemanketo.com/caveman-chili/'
         },
         {
-            "name": "Broccoli Stirfry",
-            "ingredients": [
-                "Chicken",
-                "Broccoli",
-                "Soy Sauce",
-                "Garlic"
-            ]
+            name: 'Crock Pot Crack Chicken',
+            active: false,
+            count: 0,
+            ingredients: [
+                'bacon',
+                'chicken',
+                'cream cheese',
+                'ranch seasoning',
+            ],
+            link: 'http://cookiesandcups.com/slow-cooker-crack-chicken/'
         },
         {
-            "name": "Burgers",
-            "ingredients": [
-                "Beef Patties",
-                "Cheese",
-                "Vegetable of Choice"
-            ]
+            name: 'Curry with riced Cauliflower',
+            active: false,
+            count: 0,
+            ingredients: [
+                'butter',
+                'cauliflower',
+                'chicken',
+                'curry paste',
+                'half and half',
+            ],
+            link: 'http://cavemanketo.com/curry-chicken-with-riced-cauliflower/'
+        },
+        {
+            name: 'Heroin Chicken',
+            active: false,
+            count: 0,
+            ingredients: [
+                'chicken (bone in)',
+                'parmesean',
+            ],
+            link: 'http://imgur.com/NRTV5KW'
+        },
+        {
+            name: 'Parmesean Crusted Pork Chops',
+            active: false,
+            count: 0,
+            ingredients: [
+                'almond meal',
+                'eggs',
+                'parmesean',
+                'pork chops',
+            ],
+            link: 'http://cavemanketo.com/parmesan-encrusted-pork-chops/'
+        },
+        {
+            name: 'Pizza (almond crust)',
+            active: false,
+            count: 0,
+            ingredients: [
+                'alfredo or low carb pizza sauce',
+                'almond meal',
+                'butter',
+                'cheese',
+                'eggs'
+            ],
+            link: 'http://cavemanketo.com/holy-grail-almond-bun-pizza/'
+        },
+        {
+            name: 'Tuna Avacado Salad',
+            active: false,
+            count: 0,
+            ingredients: [
+                'avacado',
+                'tuna',
+            ],
+            link: 'http://deliciousbydre.com/tuna-avocado-salad/'
+        },
+        {
+            name: 'Vegetti Alfredo',
+            active: false,
+            count: 0,
+            ingredients: [
+                'butter',
+                'cream cheese',
+                'garlic',
+                'half & half',
+                'parmesean',
+                'zuccini',
+            ],
+            link: null
         }
-    ]
+    ];
 }
